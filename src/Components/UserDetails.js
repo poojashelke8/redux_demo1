4e930b45d3003ebe4994f465e91b4ddecc8fa4cb
poojashelke8 <poojabshelke0807@gmail.com>
2024-06-03 22:32:23 +0530

    started with redux

---
+++ src/Components/UserDetails.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserDetails = () => {
+  return (
+    <div>
+        <h2>List of Users</h2>
+        <input placeholder='Enter user details'/>
+        <button>Add User</button> 
+            <li>User 1</li>
+            <li>User 2</li>
+    </div>
+  )
+}
+
+export default UserDetails
