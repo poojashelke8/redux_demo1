e1b469961c2f18a64480034f0a058a289a7c24c0
poojashelke8 <poojabshelke0807@gmail.com>
2024-06-05 21:53:04 +0530

    redux_2

--- src/Components/UserDetails.js
+++ src/Components/UserDetails.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import {useDispatch} from "react-redux"
+import { addUsers } from '../Store/Slices/UserSlices';
+
 
 const UserDetails = () => {
+
+  const dispatch = useDispatch();
+
+const addUser = (payload)=>{
+  dispatch(addUsers(payload));
+  // console.log(payload)
+}
+
   return (
     <div>
         <h2>List of Users</h2>
         <input placeholder='Enter user details'/>
-        <button>Add User</button> 
-            <li>User 1</li>
-            <li>User 2</li>
+        <button onClick={()=> addUser()}>Add User</button> 
+        
     </div>
   )
 }
 
-export default UserDetails
+export default UserDetails;
--- src/Store/Slices/UserSlices.js
+++ src/Store/Slices/UserSlices.js
@@ -4,11 +4,15 @@ const userSlice = createSlice({
     name:"users",
     initialState : [],
     reducers:{
-        addUsers(state,action){},
+        addUsers(state,action){
+            state.push(action.payload)
+            console.log(action.payload)
+        },
         deleteUsers(state,action){},
     }
 })
 
-console.log(userSlice.action)
+console.log(userSlice.actions)
 
 export default userSlice.reducer;
+export const {addUsers} = userSlice.actions;
